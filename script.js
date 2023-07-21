b0723ceb2de11f8467b9f7819f3dd3b6616ce64f
Arthur Luiz <flashgames502@gmail.com>
2023-07-21 01:32:52 -0300

    update script.js - 0.3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function display_alert_yes() {
   alert("Você aceitou a transar comigo, muita fé amor!");
 
-  window.location.href = "https://www.youtube.com/watch?v=Y8Hq6XMw0MQ";
+  window.location.href = "https://soundcloud.com/srinu-1/akon-dont-matter?si=e7b74324b0644bc9b50fbbee600af538&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
 }
 
 function getRandomPosition(element) {
